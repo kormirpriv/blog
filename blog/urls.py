1f6c155849483f822ea02415dd65224f8e0a956c
MiroslawKordiak <miroslaw.kordiak@lschq.com>
2024-10-25 15:51:37 +0200

    add login logout

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path("post/new", views.post_new, name="post_new"),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/delete/<int:pk>/', views.post_delete, name='post_delete'),
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 
+LOGIN_REDIRECT_URL = '/'  # Przekierowanie po logowaniu
+LOGOUT_REDIRECT_URL = '/'  # Przekierowanie po wylogowaniu
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
